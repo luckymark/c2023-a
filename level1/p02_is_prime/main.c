fefa0c896f9b1dbfb87851c7a3adc0d9916e292f
张剑玮 <144136546+zjw131f1fc@users.noreply.github.com>
2023-09-09 17:45:14 +0800

    Update main.c

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,17 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    for(int i = 2; i <= sqrt(num); i++)
+    {
+        if(num % i == 0)
+        {
+            printf("1非素数");
+            return 0;
+        }
+    }
+    printf("2素数");
     return 0;
 }
