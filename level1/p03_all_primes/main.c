ca7cef812c2a10b36c3f6c94b8f16a55ffb7d41c
张剑玮 <144136546+zjw131f1fc@users.noreply.github.com>
2023-09-09 17:45:43 +0800

    Update main.c

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,25 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include<stdio.h>
+#include<time.h>
+#define MAX 1010
+int num[MAX];
+int main()
+{
+    int begin = clock();
+    int nownum = 2;
+    for(; nownum <= 1000; nownum++)
+    {
+        if(num[nownum] == 0)
+        {
+            printf("%d\n",nownum);
+            int i = nownum;
+            while(nownum * i <= 1000)
+            {
+                num[nownum * i] = 1;
+                i++;
+            }
+        }
+    }
+    int end = clock();
+    printf("%dms",end - begin);
     return 0;
 }
