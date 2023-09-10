68236611f90dc5d3a646447595ae9c739564e2e3
Satar07 <multya77@outlook.com>
2023-09-10 12:51:04 +0800

    上界优化

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -3,34 +3,45 @@
 #include <time.h>
 
 #if defined(WIN32)
-    #define TIMEB _timeb
-    #define ftime _ftime
+#define TIMEB _timeb
+#define ftime _ftime
 #else
-    #define TIMEB timeb
+#define TIMEB timeb
 #endif
 
-enum {MAX_NUM = 1000};
+enum {
+    MAX_NUM = 1000
+};
 
 int main() {
     time_t st_s, ed_s, elapse;
-    struct TIMEB st_ms,ed_ms;
+    struct TIMEB st_ms, ed_ms;
     ftime(&st_ms);
     time(&st_s);
 
-    int table[MAX_NUM+3] = {};
+    int table[MAX_NUM + 3] = {};
     printf("2 3 ");
-    for (int i = 6; i <= MAX_NUM; i += 6) {
-        if (table[i-1] == 0) {
-            printf("%d ", i-1);
-            for (int j = i-1; j <= MAX_NUM; j += i-1) {
+    int i;
+    for (i = 6; (i + 1) * (i + 1) <= MAX_NUM; i += 6) {
+        if (table[i - 1] == 0) {
+            printf("%d ", i - 1);
+            for (int j = i - 1; j <= MAX_NUM; j += i - 1) {
                 table[j] = 1;
             }
         }
-        if (table[i+1] == 0) {
-            printf("%d ", i+1);
-            for (int j = i+1; j <= MAX_NUM; j += i+1) {
+        if (table[i + 1] == 0) {
+            printf("%d ", i + 1);
+            for (int j = i + 1; j <= MAX_NUM; j += i + 1) {
                 table[j] = 1;
             }
+        }
+    }
+    for (; i <= MAX_NUM; i += 6) {
+        if (table[i - 1] == 0) {
+            printf("%d ", i - 1);
+        }
+        if (table[i + 1] == 0) {
+            printf("%d ", i + 1);
         }
     }
 
