353f9b38d9f948467e7c9f532c95d14f835cbf82
Satar07 <multya77@outlook.com>
2023-09-10 12:39:45 +0800

    初级埃筛实现

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,34 @@
-#include <printf.h>
+#include <stdio.h>
+#include <sys/timeb.h>
+#include <time.h>
+
+#if defined(WIN32)
+    #define TIMEB _timeb
+    #define ftime _ftime
+#else
+    #define TIMEB timeb
+#endif
 
 int main() {
-    printf("hello world!\n");
+    time_t st_s, ed_s, elapse;
+    struct TIMEB st_ms,ed_ms;
+    ftime(&st_ms);
+    time(&st_s);
+
+    int table[1003] = {};
+    for (int i = 2; i <= 1000; ++i) {
+        if (table[i] == 0) {
+            printf("%d ", i);
+            for (int j = i; j <= 1000; j += i) {
+                table[j] = 1;
+            }
+        }
+    }
+
+    ftime(&ed_ms);
+    time(&ed_s);
+
+    elapse = (ed_s * 1000 + ed_ms.millitm) - (st_s * 1000 + st_ms.millitm);
+    printf("\nelapsed time: %lldms\n", elapse);
     return 0;
 }
