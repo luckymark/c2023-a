86f2221a409feff1eac729d06da18067aa36dbc1
Satar07 <multya77@outlook.com>
2023-09-10 16:42:27 +0800

    完成要求

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,26 @@
-#include <printf.h>
+#include <stdio.h>
+
+enum {
+    FLOOR = 3
+};
+
+void mv(char now, char to, char by, int floor);
 
 int main() {
-    printf("hello world!\n");
-    return 0;
+    mv('A', 'B', 'C', FLOOR);
 }
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "misc-no-recursion"
+
+void mv(char now, char to, char by, int floor) {
+    if (floor == 1) {
+        printf("%c -> %c\n", now, to);
+        return;
+    }
+    mv(now, by, to, floor - 1);
+    mv(now, to, by, 1);
+    mv(by, to, now, floor - 1);
+}
+
+#pragma clang diagnostic pop
