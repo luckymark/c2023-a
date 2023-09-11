6db1bcc5d708b96bb558e3ed1df63328c0faec32
Satar07 <multya77@outlook.com>
2023-09-11 20:33:16 +0800

    碰撞逻辑

--- level1/p08_push_boxes/main.c
+++ level1/p08_push_boxes/main.c
@@ -4,15 +4,23 @@
 #include <stdbool.h>
 
 HANDLE console;//窗口句柄
-COORD CrPos = {2, 1};//保存光标信息
+COORD CrPos = {};//保存光标信息
 CONSOLE_SCREEN_BUFFER_INFO ScreenBufferInfo;//保存窗口信息
 
 
-enum{
+enum {
     PERSON = 6,
     WALL = 1,
     ROAD = 0,
-    TARGET = 2
+    TARGET = 2,
+    BOX = 3
+};
+
+enum {
+    Up = 1,
+    Down = 2,
+    Left = 3,
+    Right = 4
 };
 
 int HIGH = 20, WIDTH = 53;
@@ -20,7 +28,7 @@ int HIGH = 20, WIDTH = 53;
 int map[20][53];
 
 //文件
-FILE *fp=NULL;
+FILE *fp = NULL;
 
 void init();
 
@@ -33,7 +41,7 @@ void updateStatus();
 void drawWall();
 
 //判断移动是否合法
-bool inValidMove(int x, int y);
+bool inValidMove(int x, int y, int dir);
 
 void win();
 
@@ -46,10 +54,11 @@ void prin(COORD PrPos);
 //移动函数
 void Move(COORD *MoPos, int key);
 
-
 void readMap();
 
 void initPerson();
+
+void GetNewPos(COORD *pNewCoord, int key);
 
 int main() {
     init();
@@ -60,27 +69,38 @@ int main() {
     }
 }
 
-bool inValidMove(int x, int y) {
-    return map[y][x / 2] == ROAD || map[y][x / 2] == TARGET;
+bool inValidMove(int x, int y, int dir) {
+    switch (dir) {
+        case Up:
+            return map[y][x / 2] == WALL || (map[y][x / 2] == BOX && map[y - 1][x / 2] == ROAD);
+        case Down:
+            return map[y][x / 2] == WALL || (map[y][x / 2] == BOX && map[y + 1][x / 2] == ROAD);
+        case Left:
+            return map[y][x / 2] == WALL || (map[y][x / 2] == BOX && map[y][x / 2 - 1] == ROAD);
+        case Right:
+            return map[y][x / 2] == WALL || (map[y][x / 2] == BOX && map[y][x / 2 + 1] == ROAD);
+        default:
+            break;
+    }
 }
 
 
 void Move(COORD *MoPos, int key) {
     switch (key) {
-        case 72:
-            if (!inValidMove(MoPos->X, MoPos->Y - 1))break;
+        case 72://上
+            if (inValidMove(MoPos->X, MoPos->Y - 1, Up))break;
             MoPos->Y--;
             break;
-        case 75:
-            if (!inValidMove(MoPos->X - 2, MoPos->Y))break;
+        case 75://左
+            if (inValidMove(MoPos->X - 2, MoPos->Y, Left))break;
             MoPos->X -= 2;
             break;
-        case 77:
-            if (!inValidMove(MoPos->X + 2, MoPos->Y))break;
+        case 77://右
+            if (inValidMove(MoPos->X + 2, MoPos->Y, Right))break;
             MoPos->X += 2;
             break;
-        case 80:
-            if (!inValidMove(MoPos->X, MoPos->Y + 1))break;
+        case 80://下
+            if (inValidMove(MoPos->X, MoPos->Y + 1, Down))break;
             MoPos->Y++;
             break;
         default:
@@ -110,10 +130,35 @@ void win() {
 }
 
 void kbEvent() {
+    COORD nowPos = {};
     if (kbhit()) {
+        //GetNewPos(&nowPos, getch());
         cle(CrPos);//清除原有输出
         Move(&CrPos, getch());
-        prin(CrPos);
+        //prin(CrPos);
+    }
+}
+
+void GetNewPos(COORD *pNewCoord, int key) {
+    switch (key) {
+        case 72://上
+            pNewCoord->X = CrPos.X;
+            pNewCoord->Y = CrPos.Y - 1;
+            break;
+        case 75://左
+            pNewCoord->X = CrPos.X - 2;
+            pNewCoord->Y = CrPos.Y;
+            break;
+        case 77://右
+            pNewCoord->X = CrPos.X + 2;
+            pNewCoord->Y = CrPos.Y;
+            break;
+        case 80://下
+            pNewCoord->X = CrPos.X;
+            pNewCoord->Y = CrPos.Y + 1;
+            break;
+        default:
+            break;
     }
 }
 
@@ -138,7 +183,7 @@ void init() {
 void initPerson() {
     for (int i = 0; i < HIGH; ++i) {
         for (int j = 0; j < WIDTH; ++j) {
-            if (map[i][j] == PERSON){
+            if (map[i][j] == PERSON) {
                 CrPos.X = j * 2;
                 CrPos.Y = i;
                 map[i][j] = ROAD;
@@ -149,10 +194,10 @@ void initPerson() {
 
 void readMap() {
     fp = fopen("../../level1/p08_push_boxes/map/1.map", "r");
-    fscanf_s(fp,"%d %d", &WIDTH, &HIGH);
+    fscanf_s(fp, "%d %d", &WIDTH, &HIGH);
     for (int i = 0; i < HIGH; ++i) {
         for (int j = 0; j < WIDTH; ++j) {
-            fscanf_s(fp,"%d", &map[i][j]);
+            fscanf_s(fp, "%d", &map[i][j]);
         }
     }
 }
