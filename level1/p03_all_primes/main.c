c649b704f7dc99d78faf438e154e4dda31ca9e95
陈南熹 <3607924628@qq.com>
2023-09-12 21:54:17 +0800

    Chen Nanxi

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,22 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+#include <math.h>
+int Exam(int n)
+{   int i = 0;
+    for(i = 2;i<=floor(sqrt(n));i++)
+    {
+        if(n%i==0)
+            break;
+    }
+    if(i==floor(sqrt(n)+1))
+        return 1;
+    else
+        return 0;
+}
+int main()
+{   setbuf(stdout,NULL);
+    for(int i = 0;i<1001;i++)
+    {   if(Exam(i))
+            printf("%3d ",i);
+    }
     return 0;
 }
