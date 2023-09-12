01eae59a7736ca1b415814d859297059c767cf10
ycm_windows <klgjok@163.com>
2023-09-12 21:49:37 +0800

    test

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,28 @@
-#include <printf.h>
+#include <stdio.h>
+#include <windows.h>
+#define len 80
+#define sleep 50
+#define sep '-'
+
+void move(char a){
+    int left=0,right=len-1;
+    boolean s_right=1;
+    for(;;){
+        for(int i=0;i<left;i++)putchar(sep);
+        putchar(a);
+        for(int i=0;i<right;i++)putchar(sep);
+        if (s_right){
+            left++;right--;
+        }else{
+            left--;right++;
+        }
+        if (right==-1)s_right=0;
+        else if (left==-1)s_right=1;
+        Sleep(sleep);
+    }
+}
 
 int main() {
-    printf("hello world!\n");
+    move(getchar());
     return 0;
 }
