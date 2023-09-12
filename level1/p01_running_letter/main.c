fb8504414f13de9fb873a5fa9ccf2150eee9b5e7
Sorato <3331641292@qq.com>
2023-09-12 22:03:23 +0800

    第一次提交

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,22 @@
-#include <printf.h>
+#include<stdio.h>
+int main(){
+    int i,x;
+    scanf("%d",&x);
+    for(i=2;i<x;i++)
+    {
+        if(x%i==0)
+        {
+            break;
+        }
+    }
+    if(i==x)
+    {
+        printf("是素数");
+    }else
+    {
+        printf("不是素数");
+    }
 
-int main() {
-    printf("hello world!\n");
     return 0;
 }
+
