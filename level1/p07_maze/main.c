07df8930adc80aac91f5dbeffc42896aeea96b60
ycm_windows <klgjok@163.com>
2023-09-17 13:18:29 +0800

    完成了第7,9题。

--- level1/p07_maze/main.c
+++ level1/p07_maze/main.c
@@ -1,13 +1,93 @@
 #include "stdio.h"
+#include "conio.h"
+#include "stdlib.h"
 
-#define SIZE 8
+#define SIZE_X 8
+#define SIZE_Y 8
 #define PLAYER 0
 #define GATE 1
 #define EXIT 2
 #define PATH 3
 #define WALL 4
+int place_x = 2;
+int place_y = 0;
+char map[SIZE_X][SIZE_Y]={
+        {4,4,4,4,4,4,4,4},
+        {4,3,4,3,3,3,3,4},
+        {0,3,4,4,3,4,3,4},
+        {4,3,3,3,3,3,4,4},
+        {4,4,3,4,4,4,3,2},
+        {4,4,3,4,3,4,3,4},
+        {4,3,3,3,3,3,3,4},
+        {4,4,4,4,4,4,4,4}
+};
+
+void draw_map();
+char move(int delta_x, int delta_y);
 
 int main() {
-    printf("hello world!\n");
-    return 0;
+    draw_map();
+    int ch;
+    while((ch=getch())!=0x1B) /* ESC to quit, up-72 down-80 left-75 right-77 */
+    {
+        if(ch==224){
+            ch=getch();
+            char tmp=0;
+            if(ch==72)tmp=move(-1,0);
+            else if(ch==80)tmp=move(1, 0);
+            else if(ch==75)tmp=move(0,-1);
+            else if(ch==77)tmp=move(0,1);
+            if (tmp)draw_map();
+            if (tmp==2) { printf("\nYou win!\n"); break; }
+        }
+    }
+    system("pause");
+}
+
+void draw_map(){
+    system("cls");
+    for(int x=0;x<SIZE_X;x++){
+        for(int y=0;y<SIZE_Y;y++){
+            if(map[x][y]==PLAYER){printf("*");
+                continue;}
+            else if(map[x][y]==PATH||map[x][y]==GATE){ printf("\x20");
+                continue;}
+            else if(map[x][y]==EXIT){ printf("!");
+                continue;}
+            else{
+
+                //打印边框
+                if((x==0||x==SIZE_X-1)&&(y==0||y==SIZE_Y-1)) { printf("+");
+                    continue; }
+                else if(x==0||x==SIZE_X-1){printf("-");
+                    continue;}
+                else if(y==0||y==SIZE_Y-1){ printf("|");
+                    continue;}
+
+                char l_wall = (char)(map[x][y-1]==WALL);
+                char r_wall = (char)(map[x][y+1]==WALL);
+                char u_wall = (char)(map[x-1][y]==WALL);
+                char d_wall = (char)(map[x+1][y]==WALL);
+                if (l_wall&&r_wall&&!u_wall&&!d_wall){ printf("-");
+                    continue;}
+                else if(u_wall&&d_wall&&!l_wall&&!r_wall){ printf("|");
+                    continue;}
+                else { printf("+");
+                    continue;}
+            }
+        }
+        printf("\n");
+    }
+}
+
+char move(int delta_x, int delta_y){
+    int new_x = place_x+delta_x;
+    int new_y = place_y+delta_y;
+    if((new_x<0)||(new_y<0)||(new_x>=SIZE_X)||(new_y>=SIZE_Y)||map[new_x][new_y]==WALL)return(char)0;
+    char win = (char)(map[new_x][new_y]==EXIT);
+    map[place_x][place_y] = PATH;
+    map[new_x][new_y] = PLAYER;
+    place_x = new_x;
+    place_y = new_y;
+    return ++win;
 }
--- level1/p09_linked_list/main.c
+++ level1/p09_linked_list/main.c
@@ -1,6 +1,89 @@
-#include <printf.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "limits.h"
+
+struct Node {
+    struct Node * next;
+    int value;
+};
+
+struct Node* insert(struct Node* head, int index, int value){
+    struct Node *new_node=malloc(sizeof (struct Node));
+    new_node->next=NULL;
+    new_node->value=value;
+    if(index<0)index=INT_MAX;
+    else if(index==0){new_node->next=head;return new_node;}
+    struct Node* p=head;
+    while(index>1&&p->next!=NULL) { p = p->next;index-- ;}
+    new_node->next=(p->next);
+    p->next=new_node;
+    return head;
+}
+
+int search_value(struct Node* head, int value){
+    struct Node* p=head;
+    int cnt = 0;
+    while(p!=NULL&&p->value!=value) { p = p->next;cnt++; }
+    if(p==NULL)return -1;
+    else return cnt;
+}
+
+void print_link(struct Node* head){
+    struct Node* p=head;
+    while(p!=NULL){
+        printf("%d -> ", p->value);
+        p=p->next;
+    }
+    printf("\b\b\b\b\x20\x20\x20\x20\r\n");
+}
+
+struct Node* delete_index(struct Node* head, int index){
+    struct Node* p=head;
+    if(index==0)return head->next;
+    while(index>0&&p->next!=NULL)p=p->next;
+    p->next=p->next->next;
+    return head;
+}
+
+struct Node* delete_node(struct Node* head, struct Node* node){
+    if(head==node){return head->next;}
+    struct Node* p=head;
+    while(p->next!=NULL||p->next!=node)p=p->next;
+    if(p->next==NULL)return head;
+    p->next=p->next->next;
+    return head;
+}
+
+struct Node* delete_value(struct Node* head, int value){
+    if (head->value==value)return head->next;
+    struct Node* p=head;
+    while (p->next!=NULL&&p->next->value!=value)p=p->next;
+    if(p->next==NULL)return head;
+    p->next=p->next->next;
+    return head;
+}
+
+struct Node* reversed(struct Node* head){
+    struct Node* cur=head, *new_head=NULL, *q;
+    while(cur!=NULL){
+        q = cur->next;
+        cur->next=new_head;
+        new_head=cur;
+        cur=q;
+    }
+    return new_head;
+}
 
 int main() {
-    printf("hello world!\n");
+    struct Node head = {NULL,0};
+    int val;
+    do {scanf("%d", &val);insert(&head, 1, val);}
+    while (val>=0);
+    delete_value(&head, val);
+    print_link(&head);
+    struct Node* new_head = reversed(&head);
+    print_link(new_head);
+    int index = search_value(new_head, 5);
+    printf("%d", index);
     return 0;
 }
