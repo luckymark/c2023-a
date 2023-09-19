3ea122934a212c0c2032d31c70c00b08b4a85b1c
ValkyrieSamurai <2711340737@qq.com>
2023-09-19 20:25:16 +0800

    1

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,18 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+int main()
+{
+    int n;
+    int i=2;
+    scanf("%d",&n);
+    while(i<=n-1)
+    {
+        if (n % i == 0)
+        {
+            printf("%d is not a prime number", n);
+            return 0;
+        }
+        i++;
+    }
+        printf("%d is a prime number",n);
     return 0;
 }
