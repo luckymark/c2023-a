344a26fe060d518db4fd09d87554dbc747da380f
Mgepahmge <489589139@qq.com>
2023-09-19 22:24:09 +0800

    GA完成

--- level2/GA/main.c
+++ level2/GA/main.c
@@ -20,9 +20,9 @@ int pop_size = 100;
 int score[100];
 int player_x;
 int player_y;
-const int step_amount = 100;
+const int step_amount = 200;
 struct pop {
-    int step[100];
+    int step[200];
 };
 struct score {
     int sc;
@@ -115,7 +115,6 @@ void scoring(struct pop** pop,int n) {
                     move('l');
                     break;
             }
-            printf("\nIndividual: %d , Attempt: %d",i+1,j+1);
             score[i] = step_amount - j;
             if(is_win() == 1) {
                 break;
@@ -123,21 +122,110 @@ void scoring(struct pop** pop,int n) {
         }
     }
 }
-void printpop(struct pop** pop,int n) {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < step_amount; ++j) {
-            printf("%d",pop[i] -> step[j]);
+int roulette(int input[100]) {
+    int sum = 0;
+    for (int i = 0; i < 100; ++i) {
+        sum += input[i];
+    }
+    for (int i = 0; i < 100; ++i) {
+        srand(rand());
+        double probability = (input[i]/sum);
+        if ((rand()/(RAND_MAX+1.0)) <= probability) {
+            return i;
         }
-        printf("\n");
+        srand(rand());
+        return (rand()%pop_size);
     }
 }
-int main() {
-    system("pause");
-    struct pop** population = summon(pop_size);
-    scoring(population,pop_size);
-    for (int i = 0; i < pop_size; ++i) {
-        printf("%d\n",score[i]);
+int inroulette(int input[100]) {
+    int sum = 0;
+    for (int i = 0; i < 100; ++i) {
+        sum += (201-input[i]);
     }
+    for (int i = 0; i < 100; ++i) {
+        srand(rand());
+        double probability = ((201-input[i])/sum);
+        if ((rand()/(RAND_MAX+1.0)) <= probability) {
+            return i;
+        }
+        srand(rand());
+        return (rand()%pop_size);
+    }
+}
+struct pop** sift(struct pop** pop) {
+    //抽取
+    int gene_mark_1 = roulette(score);
+    int gene_mark_2 = roulette(score);
+    int *gene_1 = pop[gene_mark_1] -> step;
+    int *gene_2 = pop[gene_mark_2] -> step;
+    //交换
+    srand(rand());
+    int times = rand()%200 + 1;
+    for (int i = 0; i < times; ++i) {
+        srand(rand());
+        int mark = rand()%200;
+        int pre = gene_1[mark];
+        gene_1[mark] = gene_2[mark];
+        gene_2[mark] = pre;
+    }
+    //变异
+    float mutation_pro = 0.01f;
+    srand(rand());
+    if ((rand()/(RAND_MAX+1.0)) <= mutation_pro) {
+        srand(rand());
+        int mutation_times = rand()%200 + 1;
+        for (int i = 0; i < mutation_times; ++i) {
+            srand(rand());
+            gene_1[rand()%200] = rand()%4 +1;
+            gene_2[rand()%200] = rand()%4 +1;
+        }
+    }
+    //淘汰并继承子代
+    for (int i = 0; i < 200; ++i) {
+        pop[inroulette(score)] -> step[i] = gene_1[i];
+    }
+    for (int i = 0; i < 200; ++i) {
+        pop[inroulette(score)] -> step[i] = gene_2[i];
+    }
+    return pop;
+}
+void best(int input[100],struct pop** pop) {
+    int max = input[0];
+    int i;
+    for (i = 0; i < pop_size; ++i) {
+        if(input[i]>max) {
+            max=input[i];
+        }
+    }
+    for (i = 0; i < pop_size; ++i) {
+        if (input[i] == max) {
+            break;
+        }
+    }
+    printf("Optimal solution:\n");
+    for (int j = 0; j < (step_amount - score[i] + 1); ++j) {
+        switch (pop[i] -> step[j]) {
+            case 1:
+                printf("W ->");
+            case 2:
+                printf("S ->");
+            case 3:
+                printf("D ->");
+            case 4:
+                printf("A ->");
+        }
+    }
+};
+int main() {
+    printf("Enter the iterations: ");
+    int iterations;
+    scanf("%d",&iterations);
+    struct pop** population = summon(pop_size);
+    for (int i = 0; i < iterations; ++i) {
+        scoring(population,pop_size);
+        population = sift(population);
+    }
+    best(score,population);
     system("pause");
     return 0;
 }
