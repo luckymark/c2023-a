66062a266517c369974abd78e7217374804856c5
ifelseSorato <144085436+ifelseSorato@users.noreply.github.com>
2023-09-20 21:24:15 +0800

    Update main.c

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,29 +1,33 @@
 #include<stdio.h>
 #include<time.h>
+
 int main()
 {
-int start,last;
-start=clock();
-int first=2;
-int end=1000;
-int i,j;
-for(i=first;i<end;i++)
-{
-    int x=i;
-    for(j=2;j<x;j++)
+    double start,last;
+    start=clock();
+    int first=3;
+    int end=1000;
+    int i,j;
+    printf("2\n");
+    for(i=first;i<end;i+=2)
     {
-        if(x%j==0)
+        int x=i;
+        for(j=3;j<x;j+=2)
         {
+            if(x%j==0)
+            {
 
-            break;
+                break;
+            }
+        }
+        if(j==x)
+        {
+            printf("%d\n",j);
         }
     }
-    if(j==x)
-    {
-        printf("%d\n",j);
-    }
-}
+
     last=clock();
-    printf("耗时%d毫秒",last-start);
-return 0;
+    printf("耗时%fms",(last-start)/CLOCKS_PER_SEC);
+    return 0;
 }
+
