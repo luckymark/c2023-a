8ee495e9f8d006a1818e192a886b9cea4d6576bb
ifelseSorato <144085436+ifelseSorato@users.noreply.github.com>
2023-09-25 21:40:48 +0800

    Update main.c

--- level1/p05_encrypt_decrypt/main.c
+++ level1/p05_encrypt_decrypt/main.c
@@ -1,6 +1,37 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
-    return 0;
+#include<stdio.h>
+#include<string.h>
+#define N 52
+void encode(char str[N],int n);
+void decode(char str[N],int n);
+int main()
+{
+char str[N];
+int n;
+printf("请输入一个字符串:");
+gets(str);
+printf("请输入要移位的数字密钥:");
+scanf("%d",&n);
+encode(str,n);
+        printf("加密后的密文：%s\n",str);
+decode(str,n);
+printf("解密后的明文：%s",str);
+             return 0;
+}
+void encode(char str[N],int n)
+{int i;
+    for(i=0;i<strlen(str);i++)
+    {
+        str[i]+=n;
+        if(str[i]>'z'||str[i]>"Z")
+        str[i]-=26;
+    }
+}
+void decode(char str[N],int n)
+{int i;
+    for(i=0;i<strlen(str);i++)
+    {
+        str[i]-=n;
+        if(str[i]>'z'||str[i]>"Z")
+        str[i]-=26;
+    }
 }
