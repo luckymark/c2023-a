01c75f41f766bd3ceac3473b0c656a91a7b16a5f
Sorato <3331641292@qq.com>
2023-10-02 18:09:47 +0800

    改进算法

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,23 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+void hanoi(int n,char A,char B,char C);
+int main()
+{
+    int n;
+    char A,B,C;
+ printf("请输入黄金圆盘的个数：");
+ scanf("%d",&n);
+    hanoi( n, 'A', 'B', 'C');
     return 0;
+}
+void hanoi(int n,char A,char B,char C)
+{
+    if(n==1)
+    {
+        printf("%c->%c\n",A,C);
+
+    }else{
+        hanoi(n-1,A,C,B);
+        printf("%c->%c\n",A,C);
+        hanoi(n-1,B,A, C);
+    }
 }
