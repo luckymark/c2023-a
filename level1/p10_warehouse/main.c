bcb5ffe759aad333ab20d02253a0313148391e06
Satar07 <multya77@outlook.com>
2023-10-24 19:32:33 +0800

    进销存框架

--- level1/p10_warehouse/main.c
+++ level1/p10_warehouse/main.c
@@ -1,6 +1,59 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+void read_data();
+
+void init_screen();
+
+void show_menu();
+
+void get_instruction();
+
+bool to_quit();
+
+void core();
+
+void save_data();
 
 int main() {
-    printf("hello world!\n");
+    read_data();
+    while (true) {
+        init_screen();
+        show_menu();
+        get_instruction();
+        if (to_quit()) break;
+        core();
+    }
+    save_data();
     return 0;
+}
+
+void save_data() {
+
+}
+
+void core() {
+
+}
+
+bool to_quit() {
+    return false;
+}
+
+void get_instruction() {
+
+}
+
+void show_menu() {
+
+}
+
+void init_screen() {
+
+}
+
+void read_data() {
+
 }
