e8e5cc3dd4229ac75ec1a77687400945a393194c
Satar07 <multya77@outlook.com>
2023-10-24 15:41:53 +0800

    list框架搭建

--- level1/p09_linked_list/main.c
+++ level1/p09_linked_list/main.c
@@ -1,6 +1,49 @@
-#include <printf.h>
+#include <stddef.h>
+#include <assert.h>
+
+typedef struct {
+
+} List;
+
+
+List *create_list(int size);
+
+void set_list(int index, int data);
+
+void reverse_list(List *list);
+
+int find_list(List *list, int data);
+
+int multi_find_list(List *list, int data, int index);
 
 int main() {
-    printf("hello world!\n");
+    List *list = create_list(3);
+    set_list(1, 1);
+    set_list(2, 2);
+    set_list(3, 2);
+    reverse_list(list);
+    int index_of_2 = find_list(list, 2);
+    assert(index_of_2 == 0);
+    int index_of_second_2 = multi_find_list(list, 2, 2);
+    assert(index_of_second_2==1);
+}
+
+int multi_find_list(List *list, int data, int index) {
     return 0;
+}
+
+int find_list(List *list, int data) {
+    return 0;
+}
+
+void reverse_list(List *list) {
+
+}
+
+void set_list(int index, int data) {
+
+}
+
+List *create_list(int size) {
+    return NULL;
 }
