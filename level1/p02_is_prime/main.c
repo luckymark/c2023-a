56a93a8a31c24902a3ad50a195ba54361566ff00
ValkyrieSamurai <2711340737@qq.com>
2023-10-29 09:35:41 +0800

    1

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+void primer(int);
 int main()
 {
     int n;
-    int i=2;
-    scanf("%d",&n);
-    while(i<=n-1)
-    {
-        if (n % i == 0)
-        {
-            printf("%d is not a prime number", n);
-            return 0;
-        }
-        i++;
-    }
-        printf("%d is a prime number",n);
+    printf("Enter a number.\n");
+    scanf("%d", &n);
+    primer(n);
     return 0;
+}
+void primer(int n)
+{
+    int k;
+    for (int i=2;i<=n-1;i++)
+    {
+        if (n%i==0)
+        {
+            k=i;
+            break;
+        }
+        else k=i;
+    }
+    if (k<n-1)
+        printf("It is not a prime number.\n");
+    else
+        printf("It is a prime number.\n");
 }
